3c997b0bce4f1faa1128be4946c0dc84d861fa80
Tera Hunt <terahunt101@gmail.com>
2015-04-02 00:58:35 -0400

    Create main navigation screen.

--- app/src/main/java/aimscapstone/infectionpreventionsource/MainActivity.java
+++ app/src/main/java/aimscapstone/infectionpreventionsource/MainActivity.java
@@ -1,7 +1,7 @@
 package aimscapstone.infectionpreventionsource;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,7 +11,8 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_main_navigation);
+
     }
 
 
