02565de62c9a3eeee5aee6f0acee6d2a54d973ef
Tera Hunt <terahunt101@gmail.com>
2015-04-04 03:36:55 -0400

    Create basic navigation between existing screens.

--- app/src/main/java/aimscapstone/infectionpreventionsource/MainActivity.java
+++ app/src/main/java/aimscapstone/infectionpreventionsource/MainActivity.java
@@ -1,16 +1,77 @@
 package aimscapstone.infectionpreventionsource;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends ActionBarActivity {
 
+    private ImageButton mVideoButton;
+    private ImageButton mTrainingButton;
+    private ImageButton mResourcesButton;
+    private ImageButton mOutbreakButton;
+    private ImageButton mFavoritesButton;
+    private ImageButton mChatButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_main_navigation);
+
+        //Start video activity
+        mVideoButton = (ImageButton)findViewById(R.id.video_image_button);
+
+        mVideoButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View v)
+            {
+                Intent openVideoActivity = new Intent(MainActivity.this, VideoNavigationActivity.class);
+                startActivity(openVideoActivity);
+            }
+        });
+
+        //Start training activity
+        mTrainingButton = (ImageButton)findViewById(R.id.training_image_button);
+
+        mTrainingButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View v)
+            {
+                Intent openTrainingActivity = new Intent(MainActivity.this, TrainingNavigationActivity.class);
+                startActivity(openTrainingActivity);
+            }
+        });
+
+        //Start outbreak activity
+        mOutbreakButton = (ImageButton)findViewById(R.id.outbreak_image_button);
+
+        mOutbreakButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View v)
+            {
+                Intent openOutbreakActivity = new Intent(MainActivity.this, OutbreakActivity.class);
+                startActivity(openOutbreakActivity);
+            }
+        });
+
+        //Start resources activity
+        mResourcesButton = (ImageButton)findViewById(R.id.resources_image_button);
+
+        mResourcesButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View v)
+            {
+                Intent openResourcesActivity = new Intent(MainActivity.this, ResourcesActivity.class);
+                startActivity(openResourcesActivity);
+            }
+        });
 
     }
 
--- app/src/main/java/aimscapstone/infectionpreventionsource/VideoNavigationActivity.java
+++ app/src/main/java/aimscapstone/infectionpreventionsource/VideoNavigationActivity.java
@@ -1,12 +1,18 @@
 package aimscapstone.infectionpreventionsource;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 public class VideoNavigationActivity extends ActionBarActivity {
+
+    private ListAdapter adapter;
+    private ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +21,27 @@ public class VideoNavigationActivity extends ActionBarActivity {
 
         String[] videoTopics = {"Video 1", "Video 2", "Video 3", "Video 4", "Video 5", "Video 6"};
 
-        ListAdapter adapter = new ArrayAdapter<String>(this, R.layout.video_row_layout,
+        adapter = new ArrayAdapter<String>(this, R.layout.video_row_layout,
                 R.id.videoRowTextView, videoTopics);
-        ListView lv = (ListView) findViewById(R.id.videoListView);
+        lv = (ListView) findViewById(R.id.videoListView);
         lv.setAdapter(adapter);
+
+        //Start video playback
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = (String) adapter.getItem(position);
+                if(item.equals("Video 1")) {
+                    Intent startVideo = new Intent(VideoNavigationActivity.this, VideoActivity.class);
+                    startActivity(startVideo);
+                }
+
+
+            }
+        });
     }
+
+
+
+
 }
